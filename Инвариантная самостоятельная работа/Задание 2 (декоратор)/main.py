d8e767d2d9b973109384c5d0a3a45ec0f81c696b
Kirill Skorobogatov <Kirill9806@gmail.com>
2019-06-07 11:37:12 +0300

    Update main.py

--- Инвариантная самостоятельная работа/Задание 2 (декоратор)/main.py
+++ Инвариантная самостоятельная работа/Задание 2 (декоратор)/main.py
@@ -1,28 +1,59 @@
-import json
 from urllib.request import urlopen
 from xml.etree import ElementTree as ET
-import xmltodict
+import json
 
 
-def CurrenciesJSONData(f):
-    def wrapper(*args, **kwargs):
-        return json.dumps(f(*args, **kwargs), sort_keys=True, indent=4, ensure_ascii=False)
-    return wrapper
+class CurrenciesXMLData:
+    """Класс для получения данных о валютах с сайта Центробанка РФ"""
 
+    def __init__(self):
+        self.currencies_ids_lst = ['R01239', 'R01235', 'R01035']
 
-class CurrenciesXMLData():
-    @CurrenciesJSONData
-    def get_data(self):
+    def get_currencies(self):
         cur_res_str = urlopen("http://www.cbr.ru/scripts/XML_daily.asp")
-        root = ET.tostring(ET.parse(cur_res_str).getroot(), encoding="unicode")
-        return xmltodict.parse(root, encoding='utf-8')
+        result = {'Valute': []}
+        cur_res_xml = ET.parse(cur_res_str)
+        root = cur_res_xml.getroot()
+        valuta = root.findall('Valute')
+        for el in valuta:
+            valuta_id = el.get('ID')
+            if str(valuta_id) in self.currencies_ids_lst:
+                concrete_valuta = {}
+                concrete_valuta.update({'ID': valuta_id})
+
+                valuta_cur_val = el.find('Value').text
+                concrete_valuta.update({'Value': valuta_cur_val})
+
+                valuta_cur_name = el.find('Name').text
+                concrete_valuta.update({'Name': valuta_cur_name})
+
+                valuta_cur_namcode = el.find('NumCode').text
+                concrete_valuta.update({'NumCode': valuta_cur_namcode})
+
+                valuta_cur_charcode = el.find('CharCode').text
+                concrete_valuta.update({'CharCode': valuta_cur_charcode})
+
+                result["Valute"].append(concrete_valuta)
+        return result
+
+
+class CurrenciesJSONData:
+    """Декоратор, позволяющий преобразовывать и сохранять данные о курсах валют в формате JSON."""
+
+    def __init__(self, obj):
+        self.obj = obj
+
+    def get_currencies(self):
+        return json.dumps(self.obj.get_currencies(), sort_keys=True, indent=3, ensure_ascii=False)
 
     def serialize(self):
-        with open('file.json', 'a', encoding='utf-8') as f:
-            json.dump(json.dumps(self.get_data(), ensure_ascii=False), f)
+        with open('file.json', 'a', encoding='utf-8') as file:
+            file.write(self.get_currencies())
 
 
 if __name__ == "__main__":
     X = CurrenciesXMLData()
-    print(X.get_data())
+    X = CurrenciesJSONData(X)
+    print(X.get_currencies())
     X.serialize()
+
